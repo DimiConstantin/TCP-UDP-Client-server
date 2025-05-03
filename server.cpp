5884b7e4ca10e9c1acb9839211a44e2e87b831ba
DimiConstantin <168590061+DimiConstantin@users.noreply.github.com>
2025-05-03 18:51:11 +0300

    final fara wildcard

--- server.cpp
+++ server.cpp
@@ -19,6 +19,29 @@
 
 #define MAXCONN 512
 
+void notify_subscribers(const char *topic_name, const char *response, size_t response_len ,std::unordered_map<std::string, std::unordered_set<std::string>> &topic_to_client_ids,
+                         std::unordered_map<std::string, client_t> &id_to_client, std::vector<struct pollfd> &pfds,
+                         std::unordered_map<std::string, client_t> &id_to_client_map, std::unordered_map<int, client_t> &fd_to_client)
+{
+    auto it = topic_to_client_ids.find(topic_name);
+    if (it == topic_to_client_ids.end()) {
+        return;
+    }
+    
+    for (const auto &client_id : it->second) {
+        auto cit = id_to_client.find(client_id);
+        if (cit != id_to_client.end() && cit->second.is_connected) {
+            int client_fd = cit->second.fd;
+            send_tcp_msg(
+                client_fd,
+                TCP_MSG_SERVER_RESPONSE,
+                (const uint8_t*)response,
+                response_len
+            );
+        }
+    }
+}
+
 void run_server(int tcp_socket_fd, int udp_socket_fd, std::unordered_map<std::string, client_t> &id_to_client,
                 std::unordered_map<int, client_t> &fd_to_client, std::unordered_map<std::string, std::unordered_set<std::string>> &topic_to_client_ids)
 {
@@ -48,7 +71,7 @@ void run_server(int tcp_socket_fd, int udp_socket_fd, std::unordered_map<std::st
 
                     // disable Nagle algorithm
                     int enable = 1;
-                    int rc = setsockopt(client_fd, IPPROTO_TCP, TCP_NODELAY | SO_REUSEADDR, &enable, sizeof(int));
+                    int rc = setsockopt(client_fd, IPPROTO_TCP, TCP_NODELAY, &enable, sizeof(int));
                     DIE(rc < 0, "setsockopt");
 
                     tcp_msg_t msg;
@@ -59,7 +82,7 @@ void run_server(int tcp_socket_fd, int udp_socket_fd, std::unordered_map<std::st
                     std::string client_id = std::string((char *)msg.payload);
                     client_id[msg.len - 1] = '\0';
 
-                    std::cout << "Client ID: " << client_id << '\n';
+                    // std::cout << "Client ID: " << client_id << '\n';
 
                     // create a new client entry
                     struct client_t client;
@@ -71,7 +94,7 @@ void run_server(int tcp_socket_fd, int udp_socket_fd, std::unordered_map<std::st
                     auto it = id_to_client.find(client_id);
                     if (it != id_to_client.end() && it->second.is_connected)
                     {
-                        // std::cout << "Client " << client_id << " already connected.\n";
+                        std::cout << "Client " << client_id << " already connected." << std::endl;
                         close(client_fd);
                         continue;
                     }
@@ -80,11 +103,100 @@ void run_server(int tcp_socket_fd, int udp_socket_fd, std::unordered_map<std::st
                     fd_to_client[client_fd] = client;
                     id_to_client[client_id] = client;
                     std::cout << "New client " << client_id << " connected from "
-                              << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << '\n';
+                              << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << std::endl;
                 }
                 else if (pfds[i].fd == udp_socket_fd)
                 {
                     // handle UDP packet
+                    struct sockaddr_in client_addr;
+                    socklen_t addr_len = sizeof(client_addr);
+                    char buffer[UDP_PKT_SIZE];
+                    memset(buffer, 0, sizeof(buffer));
+                    ssize_t bytes_received = recvfrom(udp_socket_fd, buffer, UDP_PKT_SIZE, 0,
+                                                      (struct sockaddr *)&client_addr, &addr_len);
+                    DIE(bytes_received < 0, "recvfrom");
+                    
+                    topic_t *topic = (topic_t *)buffer;
+                    char *topic_name = topic->topic;
+                    unsigned int topic_type = (unsigned int)topic->type;
+                    // std::cout << "received UDP packet with topic " << topic_name << " and type " << topic_type << "and value " << topic->payload << '\n';
+                    char response[UDP_PKT_SIZE];
+                    memset(response, 0, sizeof(response));
+                    size_t response_len;
+                    switch (topic_type)
+                    {
+                    case INT:
+                     {
+                        // handle integer topic
+                        int8_t sign = (uint8_t)topic->payload[0];
+                        int aux;
+                        memcpy(&aux, topic->payload + 1, sizeof(int));
+                        int value = ntohl(aux);
+                        if (sign == 1)
+                        {
+                            value = -value;
+                        }
+                        // std::cout << "Received integer topic: " << topic_name << " with value: " << value << '\n';
+                        snprintf(response, sizeof(response), "%s:%d - %s - %s - %d", inet_ntoa(client_addr.sin_addr),
+                             ntohs(client_addr.sin_port), topic_name, "INT", value);
+                        // std::cout << "Response: " << response << '\n';
+                        break;
+                     }
+                    case SHORT_REAL:
+                    {
+                        // handle short real topic
+                        uint16_t aux;
+                        memcpy(&aux, topic->payload, sizeof(uint16_t));
+                        double value = ntohs(aux) / 100.0;
+                        // std::cout << "Received short real topic: " << topic_name << " with value: " << value << '\n';
+                        snprintf(response, sizeof(response), "%s:%d - %s - %s - %.2f", inet_ntoa(client_addr.sin_addr),
+                             ntohs(client_addr.sin_port), topic_name, "SHORT_REAL", value);
+                        // std::cout << "Response: " << response << '\n';
+                        break;
+                    }
+                    case FLOAT:
+                    {
+                        // handle float topic
+                        uint8_t sign = (uint8_t)topic->payload[0];
+                        uint32_t aux;
+                        memcpy(&aux, topic->payload + 1, sizeof(uint32_t));
+                        uint32_t num = ntohl(aux);
+                        uint8_t exponent = (uint8_t)topic->payload[5];
+                        double value = (double)num;
+                        while (exponent--)
+                            value /= 10.0;
+                        if (sign == 1)
+                        {
+                            value = -value;
+                        }
+
+                        // std::cout << "Received float topic: " << topic_name << " with value: " << value << '\n';
+                        snprintf(response, sizeof(response), "%s:%d - %s - %s - %.15g", inet_ntoa(client_addr.sin_addr),
+                             ntohs(client_addr.sin_port), topic_name, "FLOAT", value);
+                        // std::cout << "Response: " << response << '\n';
+                        break;
+                    }
+                    case STRING:
+                    {
+                        // handle string topic
+                        std::string res(topic->payload);
+                        res[res.size()] = '\0';
+                        // std::cout << "Received string topic: " << topic_name << " with value: " << res << '\n';
+                        snprintf(response, sizeof(response), "%s:%d - %s - %s - %s", inet_ntoa(client_addr.sin_addr),
+                             ntohs(client_addr.sin_port), topic_name, "STRING", res.c_str());
+                        // std::cout << "Response: " << response << '\n';
+                        break;
+                    }
+                    default:
+                        std::cout << "Unknown topic type: " << topic_type << std::endl;
+                        break;
+                    }
+                    
+                    // char response[UDP_PKT_SIZE];
+                    response_len = strlen(response);
+                    response[response_len] = '\0';
+                    notify_subscribers(topic_name, response, response_len, topic_to_client_ids, id_to_client, pfds, id_to_client, fd_to_client);
+                             
                 }
                 else if (pfds[i].fd == STDIN_FILENO)
                 {
@@ -151,6 +263,22 @@ void run_server(int tcp_socket_fd, int udp_socket_fd, std::unordered_map<std::st
                     {
                         // handle unsubscribe message
                         // std::cout << "Client " << client.id << " unsubscribed from topic: " << packet.payload << '\n';
+                        std::string topic;
+                        size_t len = ntohs(packet.len) - 1;
+                        topic.assign((char *)packet.payload, len);
+                        // std::cout << "Unsubscribing from topic " << topic << "\n";
+                        client.topics.erase(topic);
+                        topic_to_client_ids[topic].erase(client.id);
+                        break;
+                    }
+                    case TCP_MSG_EXIT:
+                    {
+                        // handle exit message
+                        std::cout << "Client " << client.id << " disconnected." << std::endl;
+                        client.is_connected = false;
+                        id_to_client[client.id].is_connected = false;
+                        close(client_fd);
+                        pfds.erase(pfds.begin() + i);
                         break;
                     }
                     default:
@@ -171,12 +299,17 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    std::unordered_map<std::string, client_t> id_to_client;
-    std::unordered_map<int, client_t> fd_to_client;
-    std::unordered_map<std::string, std::unordered_set<std::string>> topic_to_client_ids;
+    
+    std::ios::sync_with_stdio(true);
+    // fiecare << pe cout face și flush automat
+    std::cout.setf(std::ios::unitbuf);
 
     // deactivate stdout buffering
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+
+    std::unordered_map<std::string, client_t> id_to_client;
+    std::unordered_map<int, client_t> fd_to_client;
+    std::unordered_map<std::string, std::unordered_set<std::string>> topic_to_client_ids;
 
     // read port from command line
     uint16_t port;
@@ -201,7 +334,7 @@ int main(int argc, char *argv[])
 
     // set socket options
     int enable = 1;
-    rc = setsockopt(tcp_socket_fd, IPPROTO_TCP, TCP_NODELAY | SO_REUSEADDR, (char *)&enable, sizeof(int));
+    rc = setsockopt(tcp_socket_fd, IPPROTO_TCP, TCP_NODELAY, (char *)&enable, sizeof(int));
     DIE(rc < 0, "setsockopt tcp");
 
     rc = bind(tcp_socket_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
@@ -220,6 +353,9 @@ int main(int argc, char *argv[])
 
     rc = close(udp_socket_fd);
     DIE(rc < 0, "close udp socket");
+
+    rc = close(STDIN_FILENO);
+    DIE(rc < 0, "close stdin");
 
     return 0;
 }
